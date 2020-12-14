31d2052affbb064c3ab607fb462741d442b3ec21
Mayank Bansal <sunny.bansal@gmail.com>
2020-12-13 22:54:55 -0800

    Moar cleanup

--- Problem 14/advent14a.py
+++ Problem 14/advent14a.py
@@ -1,20 +1,16 @@
 def apply_mask(mask, num):
-	num = list(num)
-	for i in range(len(num)):
-		num[i] = mask[i] if mask[i] in ['1', '0'] else num[i]
-	return num
+	return ''.join([mask[i] if mask[i] in ['1', '0'] else num[i] for i in range(36)])
 
 
 cur_mask, memory, answer = None, {}, 0
 for line in open('input.txt', 'r').readlines():
-	instruction = line.strip()
-	if 'mask' in instruction:
-		cur_mask = instruction[7:]
-	elif 'mem' in instruction:
-		inst = instruction.replace(' ', '').split('=')
-		address = '{:036b}'.format(int(inst[0][4:len(inst[0]) - 1]))
-		bin_num = '{:036b}'.format(int(inst[1]))
-		memory[address] = ''.join(apply_mask(cur_mask, bin_num))
+	inst, val = line.strip().replace(' ', '').split('=')
+	if 'mask' in inst:
+		cur_mask = val
+	elif 'mem' in inst:
+		bin_mem = '{:036b}'.format(int(inst[4:-1]))
+		bin_num = '{:036b}'.format(int(val))
+		memory[bin_mem] = apply_mask(cur_mask, bin_num)
 
 for mem, val in memory.items():
 	answer += int(val, 2)
--- Problem 14/advent14b.py
+++ Problem 14/advent14b.py
@@ -7,21 +7,17 @@ def combinations(pattern):
 
 
 def apply_mask(mask, num):
-	num = list(num)
-	for i in range(len(num)):
-		num[i] = mask[i] if mask[i] in ['1', 'X'] else num[i]
-	return num
+	return ''.join([mask[i] if mask[i] in ['1', 'X'] else num[i] for i in range(36)])
 
 
 cur_mask, memory, answer = None, {}, 0
 for line in open('input.txt', 'r').readlines():
-	instruction = line.strip()
-	if 'mask' in instruction:
-		cur_mask = instruction[7:]
-	elif 'mem' in instruction:
-		inst = instruction.replace(' ', '').split('=')
-		bin_mem = '{:036b}'.format(int(inst[0][4:len(inst[0]) - 1]))
-		bin_num = '{:036b}'.format(int(inst[1]))
+	inst, val = line.strip().replace(' ', '').split('=')
+	if 'mask' in inst:
+		cur_mask = val
+	elif 'mem' in inst:
+		bin_mem = '{:036b}'.format(int(inst[4:-1]))
+		bin_num = '{:036b}'.format(int(val))
 		for address in combinations(apply_mask(cur_mask, bin_mem)):
 			memory[address] = bin_num
 
