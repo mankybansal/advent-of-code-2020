500760af704a63e520dce67b41fb8ebeed4babeb
Mayank Bansal <sunny.bansal@gmail.com>
2020-12-13 23:05:29 -0800

    Moar cleanup

--- Problem 14/advent14a.py
+++ Problem 14/advent14a.py
@@ -1,17 +1,11 @@
-def apply_mask(mask, num):
-	return ''.join([mask[i] if mask[i] in ['1', '0'] else num[i] for i in range(36)])
-
-
-cur_mask, memory, answer = None, {}, 0
+mask, memory, answer = None, {}, 0
 for line in open('input.txt', 'r').readlines():
 	inst, val = line.strip().replace(' ', '').split('=')
 	if 'mask' in inst:
-		cur_mask = val
+		mask = val
 	elif 'mem' in inst:
-		bin_mem = '{:036b}'.format(int(inst[4:-1]))
-		bin_num = '{:036b}'.format(int(val))
-		memory[bin_mem] = apply_mask(cur_mask, bin_num)
-
-for mem, val in memory.items():
-	answer += int(val, 2)
-print(answer)
+		mem_add = '{:036b}'.format(int(inst[4:-1]))
+		num = '{:036b}'.format(int(val))
+		masked_num = ''.join([mask[i] if mask[i] in ['1', '0'] else num[i] for i in range(36)])
+		memory[mem_add] = int(masked_num, 2)
+print(sum(memory.values()))
--- Problem 14/advent14b.py
+++ Problem 14/advent14b.py
@@ -6,21 +6,14 @@ def combinations(pattern):
 	return [''.join(lst) for lst in list(itertools.product(*replace))]
 
 
-def apply_mask(mask, num):
-	return ''.join([mask[i] if mask[i] in ['1', 'X'] else num[i] for i in range(36)])
-
-
-cur_mask, memory, answer = None, {}, 0
+mask, memory, answer = None, {}, 0
 for line in open('input.txt', 'r').readlines():
 	inst, val = line.strip().replace(' ', '').split('=')
 	if 'mask' in inst:
-		cur_mask = val
+		mask = val
 	elif 'mem' in inst:
-		bin_mem = '{:036b}'.format(int(inst[4:-1]))
-		bin_num = '{:036b}'.format(int(val))
-		for address in combinations(apply_mask(cur_mask, bin_mem)):
-			memory[address] = bin_num
-
-for mem, val in memory.items():
-	answer += int(val, 2)
-print(answer)
+		mem_add = '{:036b}'.format(int(inst[4:-1]))
+		masked_mem_add = ''.join([mask[i] if mask[i] in ['1', 'X'] else mem_add[i] for i in range(36)])
+		for address in combinations(masked_mem_add):
+			memory[address] = int(val)
+print(sum(memory.values()))
